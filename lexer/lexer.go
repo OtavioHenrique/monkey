26d5672a9d0ff4401c956fff7ac58f6b8b8dda0b
Otavio H. P. Valadares <otaviopvaladares@gmail.com>
2019-12-17 01:22:34 -0300

    readChar helper

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -7,7 +7,19 @@ type Lexer struct {
 	ch           byte
 }
 
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
